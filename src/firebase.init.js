c30fcc980812a81d5b8f7c34670c6245986a7b53
S. R. Nirob <snirob786@gmail.com>
2022-04-14 11:57:43 +0600

    Firebase Intialize

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOIjerVM_JkvWZQ6nd-wWY1xG60hIQiSQ",
+  authDomain: "lunar-box-212307.firebaseapp.com",
+  projectId: "lunar-box-212307",
+  storageBucket: "lunar-box-212307.appspot.com",
+  messagingSenderId: "710740829179",
+  appId: "1:710740829179:web:f90e833e0a7c8edf56a15e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
